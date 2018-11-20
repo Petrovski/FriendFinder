26960841979b870c6e338cacf85b12f0424f0c92
FORTRESS <alexpetroski@gmail.com>
2018-11-19 22:22:23 -0500

    Updated html routes and survey page complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,43 @@
-var friends = [],
-    matchingFriend = []
+var packArray = [{
+    "name": "Black-Baked Jackal",
+    "image": "https://vignette.wikia.nocookie.net/parody/images/4/47/Jackal.jpg/revision/latest?cb=20141011182314",
+    "scores": [2, 5, 1, 4, 5, 1, 5, 5, 4, 3 ]
+},
+{
+    "name": "Golden Jackal",
+    "image": "https://www.goldschakal.at/hide/wp-content/uploads/2017/04/Goldschakal_Janez_Tarman_full_pict-Kopie.jpg",
+    "scores": [5, 1, 3, 2, 5, 4, 1, 1, 1, 3]
+},
+{
+    "name": "Side-Striped Jackal",
+    "image": "http://www.wildlife-pictures-online.com/image-files/side-stripe-jackal_lz-4382g.jpg",
+    "scores": [4, 1, 4, 2, 2, 3, 4, 2, 5, 5]
+},
+{
+    "name": "Abyssinian Wolf",
+    "image": "https://c1.staticflickr.com/4/3844/14878188488_f91a925efa_b.jpg",
+    "scores": [1, 2, 3, 1, 2, 2, 2, 2, 3, 4]
+},
+{
+    "name": "African Golden Wolf",
+    "image": "https://c1.staticflickr.com/1/692/32373434254_00a1905fb2_b.jpg",
+    "scores": [2, 3, 3, 2, 5, 1, 3, 4, 2, 3]
+},
+{
+    "name": "Gray Wolf",
+    "image": "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/gray-wolf-closeup.adapt.945.1.jpg",
+    "scores": [5, 5, 1, 3, 5, 1, 2, 4, 1, 5]
+},
+{
+    "name": "Dingo",
+    "image": "https://www.australia.com/content/australia/en_ie/facts/pictures/australias-animal-pictures/dingo/jcr:content/image.adapt.1200.HIGH.jpg",
+    "scores": [4, 2, 5, 2, 1, 4, 1, 2, 3, 4]
+},
+{
+    "name": "Arctic Wolf",
+    "image": "http://d2ouvy59p0dg6k.cloudfront.net/img/arctic_wolf_351633.jpg",
+    "scores": [3, 4, 3, 5, 1, 5, 5, 2, 2, 3]
+}];
 
 
-module.exports = {
-    friends: friends,
-    matchingFriend: matchingFriend
-}
+module.exports = packArray;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,8 +5,10 @@ const path = require("path");
 module.exports = function (app) {
 
     app.use(express.static(path.join(__dirname, "../public")))
+
     app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public", "home.html"))
+
+        res.sendFile(path.join(__dirname, "../public", "home.html"))
 
     })
 
