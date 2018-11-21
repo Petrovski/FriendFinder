e20f0f63451f1ba26f4458e5ab9c2d0f8c16b40d
FORTRESS <alexpetroski@gmail.com>
2018-11-20 21:11:54 -0500

    Updated folder structurr, server file, and links

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express"),
       app = express();
       PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
